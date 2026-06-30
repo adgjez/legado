6cd101a670f0f938bac12b3c3a6e44cab0ab5e8b
Rimchars <rimchars@local>
2026-06-30 22:03:13 +0800

    修复发现标签 Compose 编译错误

--- app/src/main/java/io/legado/app/ui/book/explore/ExploreShowComposeScreen.kt
+++ app/src/main/java/io/legado/app/ui/book/explore/ExploreShowComposeScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.calculateBottomPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
